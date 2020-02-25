eb06cf14d4a49baa99080846adf061f3020ad0ef
Jaro <jeremyvan614@gmail.com>
2020-02-25 14:13:30 -0600

    youtube replica with react

---
+++ src/components/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import Youtube from '../api/Youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+
+class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit('lofi')
+  };
+
+  onTermSubmit = async (term) => {
+    const response = await Youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
+  };
+
+  onVideoSelect = (video) => {
+    // console.log("From the App!", video);
+    this.setState({ selectedVideo: video });
+    console.log({process});
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+
+            </div>
+            <div className="five wide column">
+              <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
